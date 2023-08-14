4d04daaf6a769e20120fbb558532f106882a0ea0
iano-iano <123099744+iano-iano@users.noreply.github.com>
2023-08-14 23:31:29 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,7 @@ int main(void)
         for (letter = 'a'; letter <= 'z'; letter++)
         {
                 if (letter == 'e' || letter == 'q')
-                {
-                	continue;
-                }
+          	        continue;
                 putchar(letter);
                  
         }
