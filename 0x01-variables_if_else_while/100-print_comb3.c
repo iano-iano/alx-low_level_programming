e49c31a46b73febeb010dbaae8715113cd85402c
iano-iano <123099744+iano-iano@users.noreply.github.com>
2023-08-15 00:18:58 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
