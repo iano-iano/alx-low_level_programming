e47aec32b4d5c8e563778c2898a05d3f54f74fa0
iano-iano <123099744+iano-iano@users.noreply.github.com>
2023-08-15 00:34:24 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ int main(void)
 		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
